3139deba214a3cb9b281ee4dba60b1ae46c4932b
Pascal Berrang <git@paberr.net>
2024-11-23 16:00:54 -0600

    BLS key uncompression testing

--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,102 @@
-use std::cell::RefCell;
+use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
+use serde::de;
+use serde::{de::Visitor, Deserialize, Serialize};
+use std::io::Cursor;
 use std::rc::Rc;
+use std::{cell::RefCell, fmt};
 use wasm_bindgen::prelude::*;
 use web_sys::{
     console, HtmlElement, HtmlInputElement, MessageEvent, Worker, WorkerOptions, WorkerType,
 };
 
-/// A number evaluation struct
-///
-/// This struct will be the main object which responds to messages passed to the
-/// worker. It stores the last number which it was passed to have a state. The
-/// statefulness is not required in this example but should show how
-/// larger, more complex scenarios with statefulness can be set up.
-#[wasm_bindgen]
-pub struct NumberEval {
-    number: i32,
+use nimiq_bls::{CompressedPublicKey, PublicKey, SecretKey};
+use nimiq_utils::key_rng::SecureGenerate;
+
+fn setup_validators(num: usize) -> CompressedKeys {
+    console::log_1(&"Create validator keys".into());
+
+    let mut validators = vec![];
+    for _ in 0..num {
+        // Make sure we only have the compressed key.
+        let bls_secret_key = SecretKey::generate_default_csprng();
+        let bls_public_key = PublicKey::from_secret(&bls_secret_key);
+        validators.push(bls_public_key.compress());
+    }
+
+    CompressedKeys(validators)
+}
+
+struct UncompressedPublicKey(PublicKey);
+
+impl Serialize for UncompressedPublicKey {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let size = CanonicalSerialize::uncompressed_size(&self.0.public_key);
+        let mut bytes = vec![0; size];
+        CanonicalSerialize::serialize_uncompressed(&self.0.public_key, &mut bytes[..]).unwrap();
+        serializer.serialize_bytes(&bytes)
+    }
+}
+
+impl<'de> Deserialize<'de> for UncompressedPublicKey {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        struct PkVisitor;
+
+        impl<'de> Visitor<'de> for PkVisitor {
+            type Value = UncompressedPublicKey;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str("a serialized representation of UncompressedPublicKey")
+            }
+
+            fn visit_bytes<E>(self, v: &[u8]) -> Result<Self::Value, E>
+            where
+                E: de::Error,
+            {
+                let reader = Cursor::new(v);
+                Ok(UncompressedPublicKey(PublicKey::new(
+                    CanonicalDeserialize::deserialize_uncompressed_unchecked(reader).unwrap(),
+                )))
+            }
+        }
+
+        deserializer.deserialize_bytes(PkVisitor)
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+struct CompressedKeys(Vec<CompressedPublicKey>);
+#[derive(Serialize, Deserialize)]
+struct UncompressedKeys(Vec<UncompressedPublicKey>);
+
+impl From<CompressedKeys> for UncompressedKeys {
+    fn from(compressed: CompressedKeys) -> Self {
+        UncompressedKeys(
+            compressed
+                .0
+                .into_iter()
+                .map(|key| UncompressedPublicKey(key.uncompress().unwrap()))
+                .collect(),
+        )
+    }
 }
 
 #[wasm_bindgen]
-impl NumberEval {
-    /// Create new instance.
-    pub fn new() -> NumberEval {
-        NumberEval { number: 0 }
-    }
+pub fn uncompress_validators(compressed: JsValue) -> JsValue {
+    console::log_1(&"Deserialize value".into());
+    let compressed: CompressedKeys = serde_wasm_bindgen::from_value(compressed).unwrap();
 
-    /// Check if a number is even and store it as last processed number.
-    ///
-    /// # Arguments
-    ///
-    /// * `number` - The number to be checked for being even/odd.
-    pub fn is_even(&mut self, number: i32) -> bool {
-        self.number = number;
-        self.number % 2 == 0
-    }
+    // Do the work.
+    console::log_1(&"Uncompress keys".into());
+    let uncompressed = UncompressedKeys::from(compressed);
 
-    /// Get last number that was checked - this method is added to work with
-    /// statefulness.
-    pub fn get_last_number(&self) -> i32 {
-        self.number
-    }
+    console::log_1(&"Serialize value".into());
+    serde_wasm_bindgen::to_value(&uncompressed).unwrap()
 }
 
 /// Run entry point for the main thread.
@@ -75,45 +134,57 @@ fn setup_input_oninput_callback(worker: Rc<RefCell<web_sys::Worker>>) {
         let document = web_sys::window().unwrap().document().unwrap();
 
         let input_field = document
-            .get_element_by_id("inputNumber")
-            .expect("#inputNumber should exist");
+            .get_element_by_id("num_keys")
+            .expect("#num_keys should exist");
         let input_field = input_field
             .dyn_ref::<HtmlInputElement>()
-            .expect("#inputNumber should be a HtmlInputElement");
+            .expect("#num_keys should be a HtmlInputElement");
 
-        // If the value in the field can be parsed to a `i32`, send it to the
-        // worker. Otherwise clear the result field.
-        match input_field.value().parse::<i32>() {
-            Ok(number) => {
-                // Access worker behind shared handle, following the interior
-                // mutability pattern.
-                let worker_handle = &*worker.borrow();
-                let _ = worker_handle.post_message(&number.into());
-                persistent_callback_handle = get_on_msg_callback();
-
-                // Since the worker returns the message asynchronously, we
-                // attach a callback to be triggered when the worker returns.
-                worker_handle
-                    .set_onmessage(Some(persistent_callback_handle.as_ref().unchecked_ref()));
-            }
+        // If the value in the field can be parsed to a `usize`, prepare the compressed keys.
+        let num_keys = match input_field.value().parse::<usize>() {
+            Ok(number) => number,
             Err(_) => {
-                document
-                    .get_element_by_id("resultField")
-                    .expect("#resultField should exist")
-                    .dyn_ref::<HtmlElement>()
-                    .expect("#resultField should be a HtmlInputElement")
-                    .set_inner_text("");
+                console::log_1(&"Error parsing value".into());
+                return;
             }
+        };
+
+        let input_field = document
+            .get_element_by_id("webworker")
+            .expect("#webworker should exist");
+        let input_field = input_field
+            .dyn_ref::<HtmlInputElement>()
+            .expect("#webworker should be a HtmlInputElement");
+
+        let in_webworker = input_field.checked();
+
+        let keys = setup_validators(num_keys);
+
+        // Access worker behind shared handle, following the interior
+        // mutability pattern.
+        if in_webworker {
+            console::log_1(&"postMessage to worker".into());
+            let worker_handle = &*worker.borrow();
+            let _ = worker_handle.post_message(&serde_wasm_bindgen::to_value(&keys).unwrap());
+            persistent_callback_handle = get_on_msg_callback();
+
+            // Since the worker returns the message asynchronously, we
+            // attach a callback to be triggered when the worker returns.
+            worker_handle.set_onmessage(Some(persistent_callback_handle.as_ref().unchecked_ref()));
+        } else {
+            console::log_1(&"uncompress locally".into());
+            let keys = UncompressedKeys::from(keys);
+            console::log_1(&format!("uncompressed {} keys", keys.0.len()).into());
         }
     });
 
-    // Attach the closure as `oninput` callback to the input field.
+    // Attach the closure as `oninput` callback to the button.
     document
-        .get_element_by_id("inputNumber")
-        .expect("#inputNumber should exist")
-        .dyn_ref::<HtmlInputElement>()
-        .expect("#inputNumber should be a HtmlInputElement")
-        .set_oninput(Some(callback.as_ref().unchecked_ref()));
+        .get_element_by_id("run")
+        .expect("#run should exist")
+        .dyn_ref::<HtmlElement>()
+        .expect("#run should be a HtmlInputElement")
+        .set_onclick(Some(callback.as_ref().unchecked_ref()));
 
     // Leaks memory.
     callback.forget();
@@ -122,19 +193,9 @@ fn setup_input_oninput_callback(worker: Rc<RefCell<web_sys::Worker>>) {
 /// Create a closure to act on the message returned by the worker
 fn get_on_msg_callback() -> Closure<dyn FnMut(MessageEvent)> {
     Closure::new(move |event: MessageEvent| {
-        console::log_2(&"Received response: ".into(), &event.data());
-
-        let result = match event.data().as_bool().unwrap() {
-            true => "even",
-            false => "odd",
-        };
-
-        let document = web_sys::window().unwrap().document().unwrap();
-        document
-            .get_element_by_id("resultField")
-            .expect("#resultField should exist")
-            .dyn_ref::<HtmlElement>()
-            .expect("#resultField should be a HtmlInputElement")
-            .set_inner_text(result);
+        console::log_1(&"Received response".into());
+        let data = event.data();
+        let keys: UncompressedKeys = serde_wasm_bindgen::from_value(data).unwrap();
+        console::log_1(&format!("Deserialized {} keys", keys.0.len()).into());
     })
 }
